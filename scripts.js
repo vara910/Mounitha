02c3dfb05bc001eb8f111676272de25d4d4a66b1
Varaprasad <131538418+vara910@users.noreply.github.com>
2024-11-13 07:44:00 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
-// Open the lightbox and display the clicked image
 function openLightbox(imgElement) {
     const lightbox = document.getElementById("lightbox");
     const lightboxImg = document.getElementById("lightbox-img");
-    lightbox.style.display = "flex"; // Show the lightbox
-    lightboxImg.src = imgElement.src; // Set the image source to the clicked image
+    lightboxImg.src = imgElement.src;
+    lightbox.style.display = "flex";
 }
 
-// Close the lightbox when clicking outside the image or on the close button
 function closeLightbox() {
     const lightbox = document.getElementById("lightbox");
-    lightbox.style.display = "none"; // Hide the lightbox
+    lightbox.style.display = "none";
 }
